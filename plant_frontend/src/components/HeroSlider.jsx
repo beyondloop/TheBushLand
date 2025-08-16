2696b51ff0f58241d44932f69285a1cf1b2a77fc
Jeevan_Bhuyan <jeevanbhuyan@gmail.com>
2025-08-16 15:26:53 +0530

    HEROsLIDER IMAGE CHANGED

--- plant_frontend/src/components/HeroSlider.jsx
+++ plant_frontend/src/components/HeroSlider.jsx
@@ -8,43 +8,43 @@ import "swiper/css/pagination";
 const slides = [
   {
     id: 1,
-    img: "src/Image/indoor.webp",
+    img: "src/Image/IndoorPlants.jpg",
     alt: "Indoor Plants Sale",
   },
-    {
+  {
     id: 2,
-    img: "src/Image/outdoor_1.jpg",
+    img: "src/Image/OutdoorPlants.jpg",
     alt: "Outdoor Plants Sale",
   },
   {
     id: 3,
-    img: "src/Image/pots.webp",
+    img: "src/Image/CeramicPot.jpeg",
     alt: "New Pots Collection",
   },
   {
     id: 4,
-    img: "src/Image/outdoor.avif",
+    img: "src/Image/GardenTools.jpg",
     alt: "Gardening Tools Offer",
   },
 ];
 
 const HeroSlider = () => {
   return (
-    <div className="w-full bg-[#02231A] h-[300px]">
+<div className="w-full bg-[#02231A] h-[150px] md:h-[250px] lg:h-[300px] object-fit">
       <Swiper
         modules={[Navigation, Pagination, Autoplay]}
         navigation
         pagination={{ clickable: true }}
         autoplay={{ delay: 3000 }}
         loop
-        className="w-full h-[300px]"
+        className="w-full h-full"
       >
         {slides.map((slide) => (
           <SwiperSlide key={slide.id}>
             <img
               src={slide.img}
               alt={slide.alt}
-              className="w-full h-auto object-contain"
+              className="w-full h-full object-cover"
             />
           </SwiperSlide>
         ))}
